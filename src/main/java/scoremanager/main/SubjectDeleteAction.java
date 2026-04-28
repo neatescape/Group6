41268768548c2309c32059e97f29c7fcfbbccbdd
yu-za-123 <fki2566255@stu.o-hara.ac.jp>
2026-04-28 10:27:58 +0900

    murakami

--- src/main/java/scoremanager/main/SubjectDeleteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteAction.java
@@ -1,3 +1,4 @@
+// murakami
 package scoremanager.main;
 
 import bean.School;
@@ -9,34 +10,40 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
+
 public class SubjectDeleteAction extends Action {
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		HttpSession session = req.getSession();
+	public void execute(
+			HttpServletRequest request, HttpServletResponse response
+		) throws Exception {
+			// cdを取得する
+			String cd = request.getParameter("cd");
+			// SubjectDaoを実体化する
+			SubjectDao sd = new SubjectDao();
 			
-		String cd = req.getParameter("cd");
-		SubjectDao sd = new SubjectDao();
-		
-		// ログイン中教師の学校を取得
-		Teacher teacher = (Teacher)session.getAttribute("user");
-		if (teacher == null) {
-			teacher = new Teacher();
-			School school = new School();
-			school.setCd("tes");
-			school.setName("テスト校");
-			teacher.setId("admin1");
-			teacher.setPassword("password");
-			teacher.setName("管理者1");
-		    teacher.setSchool(school);
-		}
-		School school = teacher.getSchool();
-		
-		// Subjectの情報を取得する
-		Subject subject = sd.get(cd, school);
-		
-		// 値をセット
-		req.setAttribute("subject_name", subject.getName());
-		req.setAttribute("subject_cd", subject.getCd());
-		
-		req.getRequestDispatcher("subject_delete.jsp").forward(req, res);
+			// Schoolを実体化する
+			// ログイン中教師の学校を取得
+			HttpSession session = request.getSession();
+			Teacher teacher = (Teacher)session.getAttribute("user");
+			if (teacher == null) {
+				teacher = new Teacher();
+				School school = new School();
+				school.setCd("tes");
+				school.setName("テスト校");
+				teacher.setId("admin1");
+				teacher.setPassword("password");
+				teacher.setName("管理者1");
+			    teacher.setSchool(school);
+			}
+			School school = teacher.getSchool();
+			
+			// Subjectの情報を取得する
+			Subject subject = sd.get(cd, school);
+			
+			// subject_nameとsubject_cdをセットする
+			request.setAttribute("subject_name", subject.getName());
+			request.setAttribute("subject_cd", subject.getCd());
+			
+			request.getRequestDispatcher("subject_delete.jsp").forward(request, response);
+
 	}
 }
