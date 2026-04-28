7935b4c9fbb7b7049bd7703661930a4332d65efb
yu-za-123 <fki2566255@stu.o-hara.ac.jp>
2026-04-28 10:28:53 +0900

    murakami

--- src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -1,6 +1,5 @@
+// murakami
 package scoremanager.main;
-
-import java.util.List;
 
 import bean.School;
 import bean.Subject;
@@ -11,70 +10,46 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
+
 public class SubjectDeleteExecuteAction extends Action {
-
-	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-
-		SubjectDao subDao = new SubjectDao();
-		String error = null; // エラーメッセージ
-		
-		// 入力値の取得
-		String cd = req.getParameter("cd");
-		String name = req.getParameter("name");
-		
-		// ログイン中教師の学校を取得
-		HttpSession session = req.getSession();
-		Teacher teacher = (Teacher)session.getAttribute("user");
-		if (teacher == null) {
-			teacher = new Teacher();
-			School school = new School();
-			school.setCd("tes");
-			school.setName("テスト校");
-			teacher.setId("admin1");
-			teacher.setPassword("password");
-			teacher.setName("管理者1");
-		    teacher.setSchool(school);
-		}
-		School school = teacher.getSchool();
-		
-		// 値をセット
-		req.setAttribute("cd", cd);
-		req.setAttribute("name", name);
-		
-		// 科目コードの一覧を参照し、科目コードが存在するか確認
-		boolean existsCd = false;
-		List<Subject> subjects = subDao.filter(teacher.getSchool());
-		for (Subject subject : subjects) {
-			if (cd.equals(subject.getCd())) {
-				existsCd = true;
-				break;
+	public void execute(
+			HttpServletRequest request, HttpServletResponse response
+		) throws Exception {
+			// cdを取得する
+			String cd = request.getParameter("cd");
+			String name = request.getParameter("name");
+			
+			// SubjectDaoを実体化する
+			SubjectDao sd = new SubjectDao();
+			
+			// ログイン中教師の学校を取得
+			HttpSession session = request.getSession();
+			Teacher teacher = (Teacher)session.getAttribute("user");
+			if (teacher == null) {
+				teacher = new Teacher();
+				School school = new School();
+				school.setCd("tes");
+				school.setName("テスト校");
+				teacher.setId("admin1");
+				teacher.setPassword("password");
+				teacher.setName("管理者1");
+			    teacher.setSchool(school);
 			}
-		}
-		
-		// 科目コードが別の画面で削除されている場合
-		if (existsCd == false) {
-			error = "科目コードが存在していません";
-			req.setAttribute("error", error);
-			req.getRequestDispatcher("SubjectDelete.action").forward(req, res);
-			return;
-		}
-		
-		// Subject インスタンス生成
-		Subject sub = new Subject();
-		sub.setCd(cd);
-		sub.setName(name);
-		sub.setSchool(school);
-		
-		// 登録処理
-		boolean result = subDao.delete(sub);
-		
-		// 結果に応じて画面遷移
-		if (result) {
-			req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
-		} else {
-			error = "変更に失敗しました";
-			req.setAttribute("error", error);
-			req.getRequestDispatcher("subjectDelete.action").forward(req, res);
-		}
+			// Schoolを実体化する
+			School school = teacher.getSchool();
+			
+			Subject subject = new Subject();
+			subject.setCd(cd);
+			subject.setName(name);
+			subject.setSchool(school);
+			
+			// deleteを実行する
+			boolean count = sd.delete(subject);
+			
+			if (count) {
+				request.getRequestDispatcher("subject_delete_done.jsp").forward(request, response);
+			} else {
+				request.getRequestDispatcher("error.jsp").forward(request, response);
+			}
 	}
 }
