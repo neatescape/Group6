48ee2a1d521f5bf146b161810cf2d4c90ecf5d9a
kawabata508 <fki2566202@stu.o-hara.ac.jp>
2026-04-28 10:17:41 +0900

    Create StudentUpdateAction.java

--- src/main/java/scoremanager/main/StudentUpdateAction.java
+++ src/main/java/scoremanager/main/StudentUpdateAction.java
@@ -1,1 +1,36 @@
+package scoremanager.main;
 
+import java.util.List;
+
+import bean.Student;
+import bean.Teacher;
+import dao.ClassNumDao;
+import dao.StudentDao;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class StudentUpdateAction extends Action {
+	
+	public void execute(
+		HttpServletRequest req, HttpServletResponse res
+	) throws Exception {
+		HttpSession session = req.getSession();
+		Teacher teacher = (Teacher)session.getAttribute("user");
+		String no=req.getParameter("no");
+		StudentDao sDao=new StudentDao();
+		Student students = sDao.get(no);
+		ClassNumDao cNumDao=new ClassNumDao();
+		
+		List<String> list = cNumDao.filter(teacher.getSchool());
+		
+		req.setAttribute("ent_year", students.getEntYear());
+		req.setAttribute("no", students.getNo());
+		req.setAttribute("name", students.getName());
+		req.setAttribute("class_num_set", list);
+		req.setAttribute("isAttend", students.isAttend());
+
+		req.getRequestDispatcher("student_update.jsp").forward(req, res);
+	}
+}
